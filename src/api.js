de46510c369e362e5f3881ac28caf173ab00d7ba
Trần Đức Thịnh <92806507+tranducthinhse@users.noreply.github.com>
2025-11-05 10:40:51 +0700

    frontendv2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://radiant-mindfulness-production.up.railway.app";
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
